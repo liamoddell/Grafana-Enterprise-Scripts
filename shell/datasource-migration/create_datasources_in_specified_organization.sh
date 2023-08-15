9964060fe5c87678d22fbec5237347aa1a1c9bf6
Liam Oddell <34241305+liamoddell@users.noreply.github.com>
2023-08-15 17:38:30 +0100

    Removing redundant ORG ID

--- shell/datasource-migration/create_datasources_in_specified_organization.sh
+++ shell/datasource-migration/create_datasources_in_specified_organization.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <GRAFANA_INSTANCE> <API_KEY> <TARGET_ORG_ID> <INPUT_JSON>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <GRAFANA_INSTANCE> <API_KEY> <INPUT_JSON>"
     exit 1
 fi
 
 GRAFANA_INSTANCE="$1"
 API_KEY="$2"
-TARGET_ORG_ID="$3"
-INPUT_JSON="$4"
+INPUT_JSON="$3"
 
 # Set API endpoint
 TARGET_DATASOURCES_ENDPOINT="${GRAFANA_INSTANCE}/api/datasources"
@@ -21,16 +20,16 @@ for datasource in $(echo "$DATA" | jq -c '.[]'); do
     datasource_name=$(echo "$datasource" | jq -r '.name')
     
     # Check if the datasource already exists in the target org
-    response=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$TARGET_DATASOURCES_ENDPOINT/name/$datasource_name" -H "Authorization: Bearer $API_KEY" -d "orgId=$TARGET_ORG_ID")
+    response=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$TARGET_DATASOURCES_ENDPOINT/name/$datasource_name" -H "Authorization: Bearer $API_KEY")
     
     if [ "$response" == "200" ]; then
-        echo "Datasource '$datasource_name' already exists in organization '$TARGET_ORG_ID'. Skipping."
+        echo "Datasource '$datasource_name' already exists in organization. Skipping."
     else
         curl_response=$(curl -s -X POST "$TARGET_DATASOURCES_ENDPOINT" -H "Authorization: Bearer $API_KEY" -H "Content-Type: application/json" -d "$datasource")
         if [[ $curl_response == *"Datasource added"* ]]; then
-            echo "Datasource '$datasource_name' added to organization '$TARGET_ORG_ID'."
+            echo "Datasource '$datasource_name' added to organization."
         else
-            echo "Failed to add datasource '$datasource_name' to organization '$TARGET_ORG_ID'."
+            echo "Failed to add datasource '$datasource_name' to organization."
             echo "$curl_response"
         fi
     fi
--- shell/datasource-migration/get_all_datasources_by_organisation.sh
+++ shell/datasource-migration/get_all_datasources_by_organisation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <GRAFANA_INSTANCE> <API_KEY> <ORG_ID> <OUTPUT_FILENAME>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <GRAFANA_INSTANCE> <API_KEY> <OUTPUT_FILENAME>"
     exit 1
 fi
 
 GRAFANA_INSTANCE="$1"
 API_KEY="$2"
-ORG_ID="$3"
-OUTPUT_FILENAME="$4"
+OUTPUT_FILENAME="$3"
 
 # Append '.json' extension to the output filename if not already present
 if [[ ! "$OUTPUT_FILENAME" == *.json ]]; then
@@ -19,7 +18,7 @@ fi
 DATASOURCES_ENDPOINT="${GRAFANA_INSTANCE}/api/datasources"
 
 # Get datasources from the API
-response=$(curl -s -X GET "$DATASOURCES_ENDPOINT" -H "Authorization: Bearer $API_KEY" -d "orgId=$ORG_ID")
+response=$(curl -s -X GET "$DATASOURCES_ENDPOINT" -H "Authorization: Bearer $API_KEY")
 
 # Save the response to the output JSON file
 echo "$response" > "$OUTPUT_FILENAME"
